b6c4673bd587bbb386db4cc11c9fd1a7effcb2b4
Conrad <vibrantclouds@protonmail.com>
2020-06-20 15:01:22 -0400

    gh-pages regeneration

--- src/sources/NHentai/NHentai.ts
+++ src/sources/NHentai/NHentai.ts
@@ -16,7 +16,7 @@ export class NHentai extends Source {
     super(cheerio)
   }
 
-  get version(): string { return '0.7.3' }
+  get version(): string { return '0.7.4' }
   get name(): string { return 'nHentai' }
   get description(): string { return 'Extension that pulls manga from nHentai' }
   get author(): string { return 'Conrad Weiser' }

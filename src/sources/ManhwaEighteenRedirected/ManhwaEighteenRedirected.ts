92d83d38349471537f3506d87853e542e0307532
Conrad <vibrantclouds@protonmail.com>
2020-06-28 20:35:42 -0400

    Manhwa-18 redirected image formation fix

--- src/sources/ManhwaEighteenRedirected/ManhwaEighteenRedirected.ts
+++ src/sources/ManhwaEighteenRedirected/ManhwaEighteenRedirected.ts
@@ -16,7 +16,7 @@ export class ManhwaEighteenRedirected extends Source {
     super(cheerio)
   }
 
-  get version(): string { return '0.6.3' }
+  get version(): string { return '0.6.4' }
   get name(): string { return 'Manhwa18 (Country-Proof) (18+)' }
   get description(): string { return 'Manhwa18 source which is guarinteed to work in countries the website is normally blocked. May be a tad slower than the original source' }
   get author(): string { return 'Conrad Weiser' }
@@ -99,7 +99,7 @@ export class ManhwaEighteenRedirected extends Source {
     let rating = $('.h0_ratings_active', $('.h0rating')).toArray().length
 
     // The redirector does something weird with the image link, make it right here
-    image = image.replace("/me/me/site", "/me")
+    image = image.replace("/me/me/site", "")
 
     return [createManga({
         id: metadata.id,
